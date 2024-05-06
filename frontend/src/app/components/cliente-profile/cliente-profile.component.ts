302dff9925c3cf4fb354017d28e862dd042f3d6a
Erick Ferreira <ferreiraerick0101@gmail.com>
2024-05-06 18:05:01 -0300

    Created UserProfile

--- frontend/src/app/components/cliente-profile/cliente-profile.component.ts
+++ frontend/src/app/components/cliente-profile/cliente-profile.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ClienteComponent } from '../cliente/cliente.component';
+import { ClientesService } from '../../services/clientes.service';
+import { Cliente } from '../../../types';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-profile',
   standalone: true,
-  imports: [],
+  imports: [ClienteComponent, CommonModule],
   templateUrl: './cliente-profile.component.html',
   styleUrl: './cliente-profile.component.scss'
 })
 export class ClienteProfileComponent {
+
+  @Input() cliente!: Cliente;
+  clienteId!: number; // Define the type of your ID
+
+  constructor(
+      private clientesService: ClientesService,
+      private route: ActivatedRoute
+
+    ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.clienteId = +params['id']; // Retrieve the ID from the URL and convert it to a number
+      this.fetchCliente(this.clienteId);
+    });
+  }
+
+
+   fetchCliente(clienteId: number) {
+     this.clientesService.getCliente(`http://localhost:8090/clientes/${clienteId}`, {})
+     .subscribe((cliente: Cliente) => {
+       console.log(cliente)
+       this.cliente = cliente;
+     });
+   }
+
+
+
 
 }
